f73b1ca5b853f5aa2879278f4dc21623667c7bd5
Alphy <aochieng@healthstrat.co.ke>
2022-01-14 23:09:46 +0300

    First Defiant File Reader Init

---
+++ FileReader.php
@@ -0,0 +1,15 @@
+<?php
+
+//Define the file location
+$filename = 'file.txt';
+
+//Invoke the file function to read the file contents and store in the $data variable
+$data = file($filename);
+
+//Loop over each read line from the variable data
+foreach ($data as $line_num=>$line)
+{
+	//find all occurrences of text in square brackets, and print that text to the console.
+    preg_match_all("/\[[^\]]*\]/", $line, $matches);
+    print_r($matches[0]);
+}
